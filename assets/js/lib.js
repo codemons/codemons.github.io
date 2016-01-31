8144f17dee83cf71c8923f80142061692ae20fe2
shivajivarma <contact@shivajivarma.com>
2016-01-31 10:29:04 +0530

    site optimized

---
+++ assets/js/lib.js
@@ -0,0 +1,13 @@
+---
+# Uglifing all library files
+---
+{% include_relative lib/materialize.js %}
+{% include_relative lib/prism/prism.js %}
+{% include_relative lib/prism/components/prism-c.min.js %}
+{% include_relative lib/prism/components/prism-python.min.js %}
+{% include_relative lib/prism/components/prism-sql.min.js %}
+{% include_relative lib/prism/components/prism-bash.min.js %}
+{% include_relative lib/owl-carousel/owl.carousel.js %}
+{% include_relative lib/wow.js %}
+{% include_relative lib/smooth-on-scroll.js %}
+{% include_relative lib/jquery.inview.min.js %}
